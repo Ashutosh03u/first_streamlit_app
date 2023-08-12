3563faad493e44c40eea2982b5084bede1526477
Ashutosh03u <64391669+Ashutosh03u@users.noreply.github.com>
2023-08-12 22:15:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title('Welcome to My First App')
-streamlit.title('now we are stated New Learning')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
