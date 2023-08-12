dbca33256b680234c118a97a0918b6d132781629
Ashutosh03u <64391669+Ashutosh03u@users.noreply.github.com>
2023-08-12 22:17:49 +0200

    Update streamlit_app.py
    new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title('Welcome to My First App')
-
+streamlit.title('Welcome to My First Apps')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
